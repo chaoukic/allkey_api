e240307db80fcf42f5ed81bda0357060e77b062b
chaoukic <sammychaouki25@gmail.com>
2021-05-14 00:10:51 -0400

    Update parameters of controller methods for tenants

--- controllers/tenant.js
+++ controllers/tenant.js
@@ -1,111 +1,152 @@
 /**
  * Create all CRUD statements for a Tenants
  */
-const helper = require("./lib/passwords");
-const findAllTenants = (call, pool) => {
+const helper = require("../lib/passwords");
+const poolObject = require("../database/db");
+const findAllTenants = (req, res, next) => {
+  const pool = poolObject.pool("tenants");
   return pool
     .query("SELECT * FROM tenants ORDER BY id ASC")
     .then((results) => {
-      return results.rows;
+      res.locals.result = {
+        status: "valid",
+        data: results.rows,
+        message:
+          "The following user has requested to fetch all the tenants in the database : " +
+          req,
+      };
+      next();
     })
     .catch((error) => {
       throw error;
     });
 };
 
-const createTenant = (call, pool) => {
-  const hash = helper.hashPassword(call.request.password);
+const createTenant = (req, res, next) => {
+  const pool = poolObject.pool("tenants");
+  const hash = helper.hashPassword(req.body.password);
   const query = {
     text: "INSERT INTO tenants (Firstname, Lastname, Phonenumber,Email, Password ) VALUES ($1,$2,$3,$4, $5) ",
     values: [
-      call.request.firstname,
-      call.request.lastname,
-      call.request.phonenumber,
-      call.request.email,
+      req.body.firstname,
+      req.body.lastname,
+      req.body.phonenumber,
+      req.body.email,
       hash,
     ],
   };
   return pool
     .query(query)
     .then((results) => {
-      return results.rows;
+      res.locals.result = {
+        status: "valid",
+        data: results.rows,
+        message:
+          "Tenant Profile with email " + req.body.email + " has been created",
+      };
+      next();
     })
     .catch((error) => {
       throw error;
     });
 };
 
-const updateTenant = (call, pool) => {
+const updateTenant = (req, res, next) => {
+  const pool = poolObject.pool("tenants");
   const query = {
     text: "UPDATE tenants SET  Firstname= $1, Lastname = $2 , Phonenumber = $3 WHERE id = $4",
     values: [
-      call.request.firstname,
-      call.request.lastname,
-      call.request.phonenumber,
-      call.request.id,
+      req.body.firstname,
+      req.body.lastname,
+      req.body.phonenumber,
+      req.body.id,
     ],
   };
   return pool
     .query(query)
     .then((results) => {
-      return results.rows;
+      res.locals.result = {
+        status: "valid",
+        data: results.rows,
+        message: "Tenant Profile with id " + req.body.id + " has been updated",
+      };
+      next();
     })
     .catch((error) => {
       throw error;
     });
 };
 
-const deleteTenant = (call, pool) => {
-  const query = {
-    text: "DELETE FROM tenants WHERE id = $1",
-    values: [call.request.id],
-  };
-  return pool
-    .query(query)
-    .then((results) => {
-      return results.rows;
-    })
-    .catch((error) => {
-      throw error;
-    });
-};
-
-const validatePhoneVerificiation = (call, pool) => {
+const validatePhoneVerificiation = (req, res, next) => {
+  const pool = poolObject.pool("tenants");
   const query = {
     text: "UPDATE tenants SET  PhoneVerification= $1 WHERE id = $2",
-    values: [true, call.request.id],
+    values: [true, req.body.id],
   };
   return pool
     .query(query)
     .then((results) => {
-      return results.rows;
+      res.locals.result = {
+        status: "valid",
+        data: results.rows,
+        message: "Your phone verification has been completed",
+      };
+      next();
     })
     .catch((error) => {
       throw error;
     });
 };
 
-const validateAdminVerification = (call, pool) => {
+const validateAdminVerification = (req, res, next) => {
+  const pool = poolObject.pool("tenants");
   const query = {
     text: "UPDATE tenants SET  AdminVerification= $1 WHERE id = $2",
-    values: [true, call.request.id],
+    values: [true, req.body.id],
   };
   return pool
     .query(query)
     .then((results) => {
-      return results.rows;
+      res.locals.result = {
+        status: "valid",
+        data: results.rows,
+        message: "Tenant has been verified by an admin",
+      };
+      next();
     })
     .catch((error) => {
       throw error;
     });
 };
 
-const findTenant = (call, pool) => {
+const findTenant = (req, res, next) => {
+  const pool = poolObject.pool("tenants");
   const query = {
     text: "Select * FROM tenants WHERE id = $1",
-    values: [call.request.id],
+    values: [req.body.id],
   };
   return pool
+    .query(query)
+    .then((results) => {
+      res.locals.result = {
+        status: "valid",
+        data: results.rows,
+        message: "Fetching tenant with the following id " + req.body.id,
+      };
+      next();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+const login = (req, res, next) => {
+  const pool = poolObject.pool("tenants");
+  const query = {
+    text: "Select password from tenants where email = $1",
+    values: [req.body.email],
+  };
+  const result = pool
     .query(query)
     .then((results) => {
       return results.rows;
@@ -113,6 +154,25 @@ const findTenant = (call, pool) => {
     .catch((error) => {
       throw error;
     });
+  if (helper.comparePassword(req.body.password, result["password"])) {
+    res.locals.result = {
+      status: "valid",
+      result: true,
+      message:
+        "Valid login for the tenant with the following email " + req.body.email,
+    };
+    next();
+  } else {
+    res.locals.result = {
+      status: "invalid",
+      error_code: 100,
+      data: false,
+      message:
+        "Invalid login for the tenant with the following email " +
+        req.body.email,
+    };
+    next();
+  }
 };
 
 module.exports = {
@@ -120,7 +180,7 @@ module.exports = {
   findAllTenants,
   createTenant,
   updateTenant,
-  deleteTenant,
   validatePhoneVerificiation,
   validateAdminVerification,
+  login,
 };

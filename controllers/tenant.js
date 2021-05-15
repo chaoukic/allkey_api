de42b83c47a02e0ea761d8e6cde73b312806136c
chaoukic <sammychaouki25@gmail.com>
2021-05-15 01:23:35 -0400

    Update db to mysql and move to a async/await approach

--- controllers/tenant.js
+++ controllers/tenant.js
@@ -1,177 +1,246 @@
 /**
  * Create all CRUD statements for a Tenants
+ *
+ * SMALL NOTE -- ERROR CODE 200 -- REPRESENTS A DATABASE RELATED ISSUE
+ *            -- ERROR CODE 100 -- REPRESENTS A INVALID LOGIN MADE BY A USER -- CAN BE A TENANT OR LANDLORD OR CONTRACTOR
+ *
+ *
  */
 const helper = require("../lib/passwords");
 const poolObject = require("../database/db");
-const findAllTenants = (req, res, next) => {
-  const pool = poolObject.pool("tenants");
-  return pool
-    .query("SELECT * FROM tenants ORDER BY id ASC")
-    .then((results) => {
-      res.locals.result = {
-        status: "valid",
-        data: results.rows,
-        message:
-          "The following user has requested to fetch all the tenants in the database : " +
-          req,
-      };
-      next();
-    })
-    .catch((error) => {
-      throw error;
-    });
-};
-
-const createTenant = (req, res, next) => {
-  const pool = poolObject.pool("tenants");
-  const hash = helper.hashPassword(req.body.password);
-  const query = {
-    text: "INSERT INTO tenants (Firstname, Lastname, Phonenumber,Email, Password ) VALUES ($1,$2,$3,$4, $5) ",
-    values: [
-      req.body.firstname,
-      req.body.lastname,
-      req.body.phonenumber,
-      req.body.email,
-      hash,
-    ],
-  };
-  return pool
-    .query(query)
-    .then((results) => {
-      res.locals.result = {
-        status: "valid",
-        data: results.rows,
-        message:
-          "Tenant Profile with email " + req.body.email + " has been created",
-      };
-      next();
-    })
-    .catch((error) => {
-      throw error;
-    });
-};
-
-const updateTenant = (req, res, next) => {
-  const pool = poolObject.pool("tenants");
-  const query = {
-    text: "UPDATE tenants SET  Firstname= $1, Lastname = $2 , Phonenumber = $3 WHERE id = $4",
-    values: [
-      req.body.firstname,
-      req.body.lastname,
-      req.body.phonenumber,
-      req.body.id,
-    ],
-  };
-  return pool
-    .query(query)
-    .then((results) => {
-      res.locals.result = {
-        status: "valid",
-        data: results.rows,
-        message: "Tenant Profile with id " + req.body.id + " has been updated",
-      };
-      next();
-    })
-    .catch((error) => {
-      throw error;
-    });
-};
-
-const validatePhoneVerificiation = (req, res, next) => {
-  const pool = poolObject.pool("tenants");
-  const query = {
-    text: "UPDATE tenants SET  PhoneVerification= $1 WHERE id = $2",
-    values: [true, req.body.id],
-  };
-  return pool
-    .query(query)
-    .then((results) => {
-      res.locals.result = {
-        status: "valid",
-        data: results.rows,
-        message: "Your phone verification has been completed",
-      };
-      next();
-    })
-    .catch((error) => {
-      throw error;
-    });
-};
-
-const validateAdminVerification = (req, res, next) => {
-  const pool = poolObject.pool("tenants");
-  const query = {
-    text: "UPDATE tenants SET  AdminVerification= $1 WHERE id = $2",
-    values: [true, req.body.id],
-  };
-  return pool
-    .query(query)
-    .then((results) => {
-      res.locals.result = {
-        status: "valid",
-        data: results.rows,
-        message: "Tenant has been verified by an admin",
-      };
-      next();
-    })
-    .catch((error) => {
-      throw error;
-    });
-};
-
-const findTenant = (req, res, next) => {
-  const pool = poolObject.pool("tenants");
-  const query = {
-    text: "Select * FROM tenants WHERE id = $1",
-    values: [req.body.id],
-  };
-  return pool
-    .query(query)
-    .then((results) => {
-      res.locals.result = {
-        status: "valid",
-        data: results.rows,
-        message: "Fetching tenant with the following id " + req.body.id,
-      };
-      next();
-    })
-    .catch((error) => {
-      throw error;
-    });
-};
-
-const login = (req, res, next) => {
-  const pool = poolObject.pool("tenants");
-  const query = {
-    text: "Select password from tenants where email = $1",
-    values: [req.body.email],
-  };
-  const result = pool
-    .query(query)
-    .then((results) => {
-      return results.rows;
-    })
-    .catch((error) => {
-      throw error;
-    });
-  if (helper.comparePassword(req.body.password, result["password"])) {
+const findAllTenants = async (req, res, next) => {
+  try {
+    const pool = poolObject.pool("users");
+    const results = await pool.query("SELECT * FROM tenants ORDER BY id ASC");
     res.locals.result = {
       status: "valid",
-      result: true,
+      data: results.rows,
       message:
-        "Valid login for the tenant with the following email " + req.body.email,
+        "The following user has requested to fetch all the tenants in the database : " +
+        req,
     };
     next();
-  } else {
+  } catch (error) {
     res.locals.result = {
       status: "invalid",
-      error_code: 100,
-      data: false,
+      responseStatus: 500,
+      error_code: 200,
+      data: "An error occured ",
       message:
-        "Invalid login for the tenant with the following email " +
-        req.body.email,
+        "An issue happened while fetching all the tenants from the USERS database  \n" +
+        "The error is " +
+        error +
+        "  \n" +
+        "The request was made by the following : " +
+        req,
     };
     next();
+  }
+};
+
+const createTenant = async (req, res, next) => {
+  try {
+    const pool = poolObject.pool("users");
+    const hash = await helper.hashPassword(req.body.password);
+    const results = await pool.execute(
+      "INSERT INTO tenants (Firstname, Lastname, Phonenumber,Email, Password ) VALUES (?,?,?,?,?)",
+      [
+        req.body.firstname,
+        req.body.lastname,
+        req.body.phonenumber,
+        req.body.email,
+        hash,
+      ]
+    );
+    res.locals.result = {
+      status: "valid",
+      data: results.rows,
+      message:
+        "Tenant Profile with email " + req.body.email + " has been created",
+    };
+    next();
+  } catch (error) {
+    res.locals.result = {
+      status: "invalid",
+      responseStatus: 500,
+      error_code: 200,
+      data: "An error occured ",
+      message:
+        "An issue happened while trying to create a tenant \n" +
+        "The error is " +
+        error +
+        "  \n" +
+        "The request body contained the following : " +
+        req.body,
+    };
+    next();
+  }
+};
+
+const updateTenant = async (req, res, next) => {
+  try {
+    const pool = poolObject.pool("users");
+    const results = await pool.execute(
+      "UPDATE tenants SET  Firstname= ?, Lastname = ? , Phonenumber = ? WHERE id = ?",
+      [req.body.firstname, req.body.lastname, req.body.phonenumber, req.body.id]
+    );
+    res.locals.result = {
+      status: "valid",
+      data: results.rows,
+      message: "Tenant Profile with id " + req.body.id + " has been updated",
+    };
+    next();
+  } catch (error) {
+    res.locals.result = {
+      status: "invalid",
+      responseStatus: 500,
+      error_code: 200,
+      data: "An error occured ",
+      message:
+        "An issue happened while trying to updating a tenant \n" +
+        "The error is " +
+        error +
+        "  \n" +
+        "The request body contained the following : " +
+        req.body,
+    };
+    next();
+  }
+};
+
+const validatePhoneVerificiation = async (req, res, next) => {
+  try {
+    const pool = poolObject.pool("users");
+    const results = await pool.execute(
+      "UPDATE tenants SET  PhoneVerification= $1 WHERE id = $2",
+      [true, req.body.id]
+    );
+    res.locals.result = {
+      status: "valid",
+      data: results.rows,
+      message: "Your phone verification has been completed",
+    };
+    next();
+  } catch (error) {
+    res.locals.result = {
+      status: "invalid",
+      responseStatus: 500,
+      error_code: 200,
+      data: "An error occured ",
+      message:
+        "An issue happened while trying to validate the phone verification of  a tenant \n" +
+        "The error is " +
+        error +
+        "  \n" +
+        "The request body contained the following : " +
+        req.body,
+    };
+    next();
+  }
+};
+
+const validateAdminVerification = async (req, res, next) => {
+  try {
+    const pool = poolObject.pool("users");
+    const results = await pool.execute(
+      "UPDATE tenants SET  AdminVerification= ? WHERE id = ?",
+      [true, req.body.id]
+    );
+    res.locals.result = {
+      status: "valid",
+      data: results.rows,
+      message: "Tenant has been verified by an admin",
+    };
+    next();
+  } catch (error) {
+    res.locals.result = {
+      status: "invalid",
+      responseStatus: 500,
+      error_code: 200,
+      data: "An error occured ",
+      message:
+        "An issue happened while trying to validate the admin verification of a tenant \n" +
+        "The error is " +
+        error +
+        "  \n" +
+        "The request body contained the following : " +
+        req.body,
+    };
+    next();
+  }
+};
+
+const findTenant = async (req, res, next) => {
+  try {
+    const pool = poolObject.pool("users");
+    const results = pool.query("Select * FROM tenants WHERE id = ?", [
+      req.body.id,
+    ]);
+    res.locals.result = {
+      status: "valid",
+      data: results.rows,
+      message: "Fetching tenant with the following id " + req.body.id,
+    };
+    next();
+  } catch (error) {
+    res.locals.result = {
+      status: "invalid",
+      responseStatus: 500,
+      error_code: 200,
+      data: "An error occured ",
+      message:
+        "An issue happened while fetching a tenant from the USERS database  \n" +
+        "The error is " +
+        error +
+        "  \n" +
+        "The request was made by the following : " +
+        req,
+    };
+    next();
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const pool = poolObject.pool("users");
+    const result = pool.query("Select password from tenants where email = $1", [
+      req.body.email,
+    ]);
+    if (helper.comparePassword(req.body.password, result["password"])) {
+      res.locals.result = {
+        status: "valid",
+        data: true,
+        message:
+          "Valid login for the tenant with the following email " +
+          req.body.email,
+      };
+      next();
+    } else {
+      res.locals.result = {
+        status: "invalid",
+        responseStatus: 403,
+        error_code: 100,
+        data: false,
+        message:
+          "Invalid login for the tenant with the following email " +
+          req.body.email,
+      };
+      next();
+    }
+  } catch (error) {
+    res.locals.result = {
+      status: "invalid",
+      responseStatus: 500,
+      error_code: 200,
+      data: "An error occured ",
+      message:
+        "An issue happened while validating the login functionality  \n" +
+        "The error is " +
+        error +
+        "  \n" +
+        "The request was made by the following : " +
+        req,
+    };
   }
 };
 
--- database/db.js
+++ database/db.js
@@ -1,12 +1,14 @@
-const Pool = require("pg").Pool;
+const mysql = require("mysql2/promise");
 
 const pool = (database_name) => {
-  return new Pool({
+  return mysql.createPool({
     user: process.env.DATABASE_USER,
     host: process.env.DATABASE_HOST,
     database: database_name,
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
   });
 };
 
